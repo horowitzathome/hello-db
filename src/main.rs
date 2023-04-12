ab209fc80dc4efa3432f22c407c8ac091e8189eb
horowitzathome <georg.nozicka@gmail.com>
2023-04-12 14:43:45 +0200

    Added test for postgres env

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,26 @@ mod tests {
     fn test() {
         println!("Test");
     }
+
+    #[test]
+    fn read_env() {
+        use dotenv::dotenv;
+
+        dotenv().ok();
+
+        let user = std::env::var("USER").expect("USER must be set.");
+
+        println!("User = {}", user);
+    }
+
+    #[test]
+    fn read_postgres_env() {
+        use dotenv::dotenv;
+
+        dotenv().ok();
+
+        let user_res = std::env::var("DATABASE_URL");
+
+        println!("User = {:?}", user_res);
+    }
 }
