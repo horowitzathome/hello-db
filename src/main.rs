e565a413305f21b158b780266d0bbffe9d85352d
horowitzathome <georg.nozicka@gmail.com>
2023-04-13 08:22:00 +0200

    Added Diesel setup

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,38 @@
+//#[macro_use]
+//extern crate diesel;
+//#[macro_use]
+//extern crate diesel_migrations;
+
+use dotenv::dotenv;
+
+mod db_infra_pool;
+mod models;
+mod schema;
+
+// docker run -itd -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -v /Users/georg/Postgres/data_test:/var/lib/postgresql/data --name postgresqltest postgres
+
 fn main() {
     println!("Hello, world!");
+    dotenv().ok();
+    db_infra_pool::init();
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::db_infra_pool;
+    use dotenv::dotenv;
 
     #[test]
-    fn test() {
-        println!("Test");
-    }
-
-    #[test]
-    fn read_env() {
-        use dotenv::dotenv;
-
+    fn test_db_url() {
         dotenv().ok();
-
-        let user = std::env::var("USER").expect("USER must be set.");
-
-        println!("User = {}", user);
-    }
-
-    #[test]
-    fn read_postgres_env() {
-        use dotenv::dotenv;
-
-        dotenv().ok();
-
         let pg_res = std::env::var("DATABASE_URL");
 
         println!("Postgres URL = {:?}", pg_res);
+    }
 
-        if let Ok(pg) = pg_res {
-            println!("PG URL = {}", pg);
-        }
+    #[test]
+    fn try_db_init() {
+        dotenv().ok();
+        db_infra_pool::init();
     }
 }
